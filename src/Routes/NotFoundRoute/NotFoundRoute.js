b029a844ea1d9836408adafaac7167c4f50e5931
Hunter Kreshock <hkreshock@gmail.com>
2019-04-24 10:51:03 -0700

    css

--- src/Routes/NotFoundRoute/NotFoundRoute.js
+++ src/Routes/NotFoundRoute/NotFoundRoute.js
@@ -1,14 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import "./NotFoundRoute.css";
 
 class NotFoundRoute extends Component {
-    render() {
-        return (
-            <section>
-                <h2>404 - Page not found</h2>
-                <p>Try going back to your previous page.</p>
-            </section>
-        );
-    }
+  render() {
+    return (
+      <section className="notFound">
+        <h2>404 - Page not found</h2>
+        <p>Try going back to your previous page.</p>
+      </section>
+    );
+  }
 }
 
-export default NotFoundRoute
+export default NotFoundRoute;
