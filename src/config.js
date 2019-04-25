3ccef75e035cf4b64b4574b741641a1537f902d1
matth3wn <matth3wn@gmail.com>
2019-04-25 09:33:57 -0700

    fixed config

--- src/config.js
+++ src/config.js
@@ -1,11 +1,5 @@
 export default {
-<<<<<<< HEAD
-    API_ENDPOINT: 'http://localhost:8000/api'
-}
-=======
-    API_ENDPOINT: 'http://localhost:4390/api',
+    API_ENDPOINT: 'http://localhost:8000/api',
     SLACK_KEY: process.env.REACT_APP_SLACK_TOCKEN,
-    SLACK_API_ENDPOINT: 'http://b24e516c.ngrok.io'
   }
   
->>>>>>> send-message
