c6d2462109b5e29711cbb81a6461084148851eae
matth3wn <matth3wn@gmail.com>
2019-04-26 16:01:49 -0700

    taking out res.json from patch

--- src/services/api-service.js
+++ src/services/api-service.js
@@ -23,11 +23,6 @@ const ApiService = {
         'authorization': `Bearer ${TokenService.getAuthToken()}`
       }
     })
-      .then(res =>
-        (!res.ok)
-          ? res.json().then(e => Promise.reject(e))
-          : res.json()
-      ) 
   },
 
   addStudent(description) {
