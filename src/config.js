623b85ab1aabf8e696707b2b9ad9a74c3d58892a
Jonathan Kleinpeter <joklein341@gmail.com>
2019-04-29 08:30:05 -0500

    add error boundary

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    API_ENDPOINT: 'http://localhost:8000/api',
+    API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT /* || 'http://localhost:8000/api' */,
     SLACK_KEY: process.env.REACT_APP_SLACK_TOCKEN,
-  }
+}
   
--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import "./index.css";
 import App from "./components/App/App";
 import { BrowserRouter } from "react-router-dom";
 import { UserProvider } from "./context/UserContext";
+import ErrorBoundary from './components/ErrorBoundary/ErrorBoudary';
 
 ReactDOM.render(
   <BrowserRouter>
     <UserProvider>
-      <App />
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
     </UserProvider>
   </BrowserRouter>,
   document.getElementById("root")
