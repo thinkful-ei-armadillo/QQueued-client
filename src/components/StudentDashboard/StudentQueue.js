30ac31727ed3fcd23c7f13ad0661e798fbad36f3
Hunter Kreshock <hkreshock@gmail.com>
2019-05-01 14:38:45 -0700

    student queue

--- src/components/StudentDashboard/StudentQueue.js
+++ src/components/StudentDashboard/StudentQueue.js
@@ -3,7 +3,7 @@ import QueueContext from "../../context/QueueContext";
 import HelpForm from "../../components/HelpForm/HelpForm";
 import "./StudentQueue.css";
 import StudentWaitingNameList from "./studentWaitingNameList/studentWatingNameList";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export default class StudentQueue extends Component {
   static contextType = QueueContext;
@@ -12,17 +12,21 @@ export default class StudentQueue extends Component {
     this.context.webSocket();
     this.context.dequeueWait();
   }
-  renderPlaceInLine(indexInLine){
-    return (<div>You are currently #{indexInLine + 1} in line.</div>)
+  renderPlaceInLine(indexInLine) {
+    return <div>You are currently #{indexInLine + 1} in line.</div>;
   }
-  renderOpenTickets(numOfTickets){
-    return (<div>You have {numOfTickets} open ticket(s).</div>)  
+  renderOpenTickets(numOfTickets) {
+    return <div>You have {numOfTickets} open ticket(s).</div>;
   }
-  renderChatRoom(room){
-    return ( 
-      <p>Your mentor {room.mentorName} is waiting in this
-        <span><Link to={room.url}> Room </Link></span>
-      </p>)
+  renderChatRoom(room) {
+    return (
+      <p>
+        Your mentor {room.mentorName} is waiting in this
+        <span>
+          <Link to={room.url}> Room </Link>
+        </span>
+      </p>
+    );
   }
   render() {
     const { showNote, queueList } = this.context;
@@ -37,18 +41,24 @@ export default class StudentQueue extends Component {
         <div className="studentsMainPage">
           {showNote.user_name === user_name && this.renderChatRoom(showNote)}
           {numberInLine > 0 && this.renderPlaceInLine(numberInLine)}
-          {userTickets ? this.renderOpenTickets(userTickets.length): 
+          {userTickets ? (
+            this.renderOpenTickets(userTickets.length)
+          ) : (
             <div>You don't have any tickets open.</div>
-          }
+          )}
           <h2 className="studentListTitle">Waiting List</h2>
           <HelpForm className="getHelpButton" />
           <ul className="studentWaitingQueue">
-            {queueList.map((listItem, index) => 
-              <StudentWaitingNameList
-                key={index}
-                personInLine={listItem}
-                currentUser={user_name}
-              />
+            {queueList.length > 0 ? (
+              queueList.map((listItem, index) => (
+                <StudentWaitingNameList
+                  key={index}
+                  personInLine={listItem}
+                  currentUser={user_name}
+                />
+              ))
+            ) : (
+              <p className="noOneInQueue">No one is in the queue</p>
             )}
           </ul>
         </div>
