810e3b0f92eefc6fc49ee6d2e2d9032c26f65b8e
Hunter Kreshock <hkreshock@gmail.com>
2019-05-03 12:10:30 -0700

    student history

--- src/Routes/StudentHistory/StudentHistoryRoute.js
+++ src/Routes/StudentHistory/StudentHistoryRoute.js
@@ -1,37 +1,20 @@
-import React, { Component } from 'react'
-import StudentHistory from '../../components/StudentHistory/StudentHistory'
-import UserContext from '../../context/UserContext';
+import React, { Component } from "react";
+import StudentHistory from "../../components/StudentHistory/StudentHistory";
+import UserContext from "../../context/UserContext";
 
 export class StudentHistoryRoute extends Component {
-  static contextType = UserContext
-  state = {
-    historyName: true
-  };
+  static contextType = UserContext;
+  state = {};
   render() {
-    const {full_name} = this.context.user;
+    const { full_name } = this.context.user;
     return (
-    <section className="row">
+      <section className="row">
         <div className="studentHistory col-12">
-        <button
-          className="buttonHistory"
-          type="button"
-          onClick={() => {
-              if (!this.state.historyName) {
-                  this.setState({ historyName: true });
-                } else {
-                    this.setState({ historyName: false });
-                }
-            }}
-            >
-          My Ticket History
-        </button>
-        <div className={this.state.historyName ? "hide" : "display"}>
           <StudentHistory currentUser={full_name} />
         </div>
-      </div>
-    </section>
-    )
+      </section>
+    );
   }
 }
 
-export default StudentHistoryRoute
+export default StudentHistoryRoute;
