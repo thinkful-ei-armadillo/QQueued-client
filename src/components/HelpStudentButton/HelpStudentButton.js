ed28777721c9bd148467959aad58a72f6a76598e
matth3wn <matth3wn@gmail.com>
2019-05-03 14:24:49 -0700

    added uuid to the parametes of the chat route

--- src/components/HelpStudentButton/HelpStudentButton.js
+++ src/components/HelpStudentButton/HelpStudentButton.js
@@ -2,18 +2,20 @@ import React, { useContext } from "react";
 import QueueContext from "../../context/QueueContext";
 import Button from "../../components/Button/Button";
 import slackService from "../../services/slack-api-service"
+import uuidv4 from "uuid";
 
 export default function HelpStudentButton(props) {
   const context = useContext(QueueContext);
   const { mentorName } = props;
   const { history } = props;
-
+  
   const handleOnClick = () => {
+    const uuid = uuidv4()
     if(context.queueList.length > 0){
     context.queueList[0].mentorName = mentorName
-    context.queueList[0].url = `/mentor/${encodeURI(mentorName)}`
+    context.queueList[0].url = `/mentor/${uuid}`
     if(context.queueList[0].slack_user_id) {
-      const text = `Your mentor is ready go at http://localhost:3000${context.queueList[0].url}`
+      const text = `Your mentor is ready go at http://localhost:3000${uuid}`
       slackService.openDmAndMessage(context.queueList[0].slack_user_id, text)
       }
       
