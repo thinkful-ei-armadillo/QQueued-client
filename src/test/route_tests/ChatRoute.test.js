22d358cd3582175210605d63bb2399af2090d872
Jonathan Kleinpeter <joklein341@gmail.com>
2019-05-06 15:28:01 -0500

    starting point for route tests

--- src/test/route_tests/ChatRoute.test.js
+++ src/test/route_tests/ChatRoute.test.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom";
 import ChatRoute from '../../Routes/ChatRoute/ChatRoute';
 
 describe("ChatRoute function component", () => {
-  it.skip("renders without crashing", () => {
+  it("renders without crashing", () => {
     const div = document.createElement('div');
     ReactDOM.render(<ChatRoute />, div);
     ReactDOM.unmountComponentAtNode(div);
