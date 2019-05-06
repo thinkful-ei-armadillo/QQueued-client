db4872e536dcb2378c7528d6c003fa9d428984a0
Jonathan Kleinpeter <joklein341@gmail.com>
2019-05-06 15:01:07 -0500

    finish studentInfo smoke test

--- src/test/StudentInfo.test.js
+++ src/test/StudentInfo.test.js
@@ -1,7 +1,48 @@
 import React from "react";
 import TestRenderer from "react-test-renderer";
 import ReactDOM from "react-dom";
+import StudentInfo from '../components/StudentInfo/StudentInfo';
 
-describe("", () => {
-  it.skip("renders without crashing", () => {});
+describe("StudentInfo function component", () => {
+  const match = {
+    isExact: true,
+    params: { studentName: "Robin" },
+    path: "/data/:studentName",
+    url: "/data/Robin"
+  }
+
+  const data = {
+    studentData: [
+      {
+        studentName: "Robin",
+        mentors: ["Dunder Mifflin Admin"],
+        questions: ["help me i dont know what im doing", "halp!"]
+      },
+      {
+        studentName: "Jonathan",
+        mentors: ["test mentor"],
+        questions: ["test q", "test q 2", "test q 3"]
+      }
+    ],
+    notes: [
+      {
+        studentName: "Hunter",
+        notes: ["note 1"]
+      },
+      {
+        studentName: "Jonathan",
+        notes: ["note 1", "note 2", "note 3"]
+      },
+      {
+        studentName: "Robin",
+        notes: []
+      }
+    ]
+  };
+
+  it.only("renders without crashing", () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<StudentInfo dataContext={data} match={match} />, div)
+    ReactDOM.unmountComponentAtNode(div);
+  });
 });
