e4e6bafaac5585d34f2b115b7b1f9529dc45c7b3
Jonathan Kleinpeter <joklein341@gmail.com>
2019-05-06 14:15:11 -0500

    fix studentDashboard tests

--- src/test/StudentDashboard.test.js
+++ src/test/StudentDashboard.test.js
@@ -1,30 +1,50 @@
 import React from "react";
 import TestRenderer from "react-test-renderer";
+import { MemoryRouter } from 'react-router-dom';
 import ReactDOM from "react-dom";
 import StudentQueue from '../components/StudentDashboard/StudentQueue';
 import StudentWaitingNameList from '../components/StudentDashboard/studentWaitingNameList/studentWaitingNameList';
 
 describe("StudentDashboard components", () => {
   let div;
+  let user;
 
   beforeEach(() => {
+    user = {
+      id: 4,
+      full_name: "Jonathan",
+      user_name: "student3",
+      title: "student"
+    };
+
     div = document.createElement("div");
   })
 
   afterEach(() => {
+    user = {};
     ReactDOM.unmountComponentAtNode(div);
   })
 
   describe('StudentQueue class component', () => {
     it("renders without crashing", () => {
-      ReactDOM.render(<StudentQueue />, div);
+      ReactDOM.render(
+        <MemoryRouter>
+          <StudentQueue user={user} />
+        </MemoryRouter>,
+        div
+      );
     });
   })
 
   describe('StudentWaitingNameList class component', () => {
     it('renders without crashing', () => {
-      ReactDOM.render(<StudentWaitingNameList />, div);
+      ReactDOM.render(
+        <MemoryRouter>
+          <StudentWaitingNameList personInLine={ user } />
+        </MemoryRouter>,
+        div
+      );
     })
   })
-  
+
 });
