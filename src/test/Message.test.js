cc4e44a02fc0c9360cb64adfa18fdcb03690d736
matth3wn <matth3wn@gmail.com>
2019-05-06 13:02:24 -0700

    added tests to the message component

--- src/test/Message.test.js
+++ src/test/Message.test.js
@@ -2,11 +2,27 @@ import React from "react";
 import TestRenderer from "react-test-renderer";
 import ReactDOM from "react-dom";
 import Message from '../components/Message/Message';
+import '../setupTests';
+import { mount } from 'enzyme';
 
 describe("Message class component", () => {
+  const wrapper = mount(<Message />)
   it("renders without crashing", () => {
     const div = document.createElement('div');
     ReactDOM.render(<Message />, div);
     ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it("renders textarea", () => {
+    expect(wrapper.find('#sendMessage'))
+  });
+  it("renders button", () => {
+    expect(wrapper.find('.slack-message-button'))
+  });
+  it("renders dropdown", () => {
+    expect(wrapper.find('.studentDropDown'))    
+  });
+  it("renders note", () => {
+    expect(wrapper.find('p').text())    
   });
 });
