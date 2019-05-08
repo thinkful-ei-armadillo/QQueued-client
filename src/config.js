6997c3e92633626b8924e856654335ec50b0ded6
matth3wn <matth3wn@gmail.com>
2019-05-08 08:19:06 -0700

    changed api_url to app_url

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
     API_ENDPOINT: /* process.env.REACT_APP_API_ENDPOINT || */ 'http://localhost:8000/api',
     SLACK_KEY: process.env.REACT_APP_SLACK_TOKEN,
-    API_URL: 'http://localhost:3000/'
+    APP_URL: 'http://localhost:3000/'
 }
   
