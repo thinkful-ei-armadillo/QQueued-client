cdbba40e86fd696a956f8f742803d01cf7c11418
matth3wn <matth3wn@gmail.com>
2019-05-09 08:56:25 -0700

    added pic to landing page

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
-    API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT  /* 'http://localhost:8000/api' */,
-    SOCKET_ENDPOINT: process.env.REACT_APP_SOCKET_ENDPOINT,
+    API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT  || 'http://localhost:8000/api' ,
+    SOCKET_ENDPOINT: process.env.REACT_APP_SOCKET_ENDPOINT || 'http://localhost:8000/',
     SLACK_KEY: process.env.REACT_APP_SLACK_TOCKEN
 }
   
